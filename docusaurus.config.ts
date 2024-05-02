77d406082db274fbaad0088b527ef007ce5987aa
Esteban Borai <estebanborai@gmail.com>
2024-05-02 17:05:29 -0400

    feat: setup search capabilities

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,81 +1,80 @@
-import { themes as prismThemes } from 'prism-react-renderer';
+import { themes as prismThemes } from "prism-react-renderer";
 
-import type {Config} from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
-const FLUVIO_REPOSITORY_URL = 'https://github.com/InfinyOn/fluvio';
+const FLUVIO_REPOSITORY_URL = "https://github.com/InfinyOn/fluvio";
 
 const config: Config = {
-  title: 'Fluvio',
-  tagline: 'The programmable data streaming platform',
-  favicon: 'img/favicon.ico',
+  title: "Fluvio",
+  tagline: "The programmable data streaming platform",
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://fluvio.io/',
+  url: "https://fluvio.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'InfinyOn', // Usually your GitHub org/user name.
-  projectName: 'fluvio', // Usually your repo name.
+  organizationName: "InfinyOn", // Usually your GitHub org/user name.
+  projectName: "fluvio", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   headTags: [
     {
-      tagName: 'link',
+      tagName: "link",
       attributes: {
-        rel: 'preconnect',
-        href: 'https://fonts.googleapis.com'
-      }
+        rel: "preconnect",
+        href: "https://fonts.googleapis.com",
+      },
     },
     {
-      tagName: 'link',
+      tagName: "link",
       attributes: {
-        rel: 'preconnect',
-        href: 'https://fonts.gstatic.com',
-      }
+        rel: "preconnect",
+        href: "https://fonts.gstatic.com",
+      },
     },
     {
-      tagName: 'link',
+      tagName: "link",
       attributes: {
-        href: 'https://fonts.googleapis.com/css2?family=Spline+Sans+Mono&display=swap',
-        rel: 'stylesheet',
-      }
+        href: "https://fonts.googleapis.com/css2?family=Spline+Sans+Mono&display=swap",
+        rel: "stylesheet",
+      },
     },
     {
-      tagName: 'link',
+      tagName: "link",
       attributes: {
-        href: 'https://fonts.googleapis.com/css2?family=Rubik&display=swap',
-        rel: 'stylesheet',
-      }
-    }
+        href: "https://fonts.googleapis.com/css2?family=Rubik&display=swap",
+        rel: "stylesheet",
+      },
+    },
   ],
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
-          sidebarPath: './sidebars.ts',
+          sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/infinyon/fluvio-docs/tree/main',
+          editUrl: "https://github.com/infinyon/fluvio-docs/tree/main",
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
@@ -83,61 +82,61 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: "img/docusaurus-social-card.jpg",
     navbar: {
       logo: {
-        alt: 'Fluvio Logo',
-        src: 'img/fluvio-with-text.svg',
-        srcDark: 'img/fluvio-with-text-white.svg',
+        alt: "Fluvio Logo",
+        src: "img/fluvio-with-text.svg",
+        srcDark: "img/fluvio-with-text-white.svg",
       },
       items: [
         {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Docs',
+          type: "docSidebar",
+          sidebarId: "tutorialSidebar",
+          position: "left",
+          label: "Docs",
         },
         {
           href: FLUVIO_REPOSITORY_URL,
-          label: 'GitHub',
-          position: 'right',
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Fluvio',
+          title: "Fluvio",
           items: [
             {
-              label: 'Docs',
-              to: '/docs/intro',
+              label: "Docs",
+              to: "/docs/quickstart",
             },
           ],
         },
         {
-          title: 'Community',
+          title: "Community",
           items: [
             {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/fluvio',
+              label: "Stack Overflow",
+              href: "https://stackoverflow.com/questions/tagged/fluvio",
             },
             {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/bBG2dTz',
+              label: "Discord",
+              href: "https://discordapp.com/invite/bBG2dTz",
             },
             {
-              label: 'Twitter',
-              href: 'https://twitter.com/infinyon',
+              label: "Twitter",
+              href: "https://twitter.com/infinyon",
             },
           ],
         },
         {
-          title: 'More',
+          title: "More",
           items: [
             {
-              label: 'GitHub',
+              label: "GitHub",
               href: FLUVIO_REPOSITORY_URL,
             },
           ],
@@ -150,6 +149,21 @@ const config: Config = {
       darkTheme: prismThemes.dracula,
     },
   } satisfies Preset.ThemeConfig,
+  themes: [
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
+      {
+        // ... Your options.
+        // `hashed` is recommended as long-term-cache of index file is possible.
+        hashed: true,
+        // For Docs using Chinese, The `language` is recommended to set to:
+        // ```
+        // language: ["en", "zh"],
+        // ```
+      },
+    ],
+  ],
 };
 
 export default config;
