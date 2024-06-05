fc46acd4e71a7a42f2df0a92b7cd310e4b1317db
Esteban Borai <estebanborai@gmail.com>
2024-06-05 17:27:50 -0400

    feat: sdk installation guides and examples

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,5 +1,3 @@
-import { themes as prismThemes } from "prism-react-renderer";
-
 import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
 
@@ -160,6 +158,8 @@ const config: Config = {
     prism: {
       additionalLanguages: [
         'bash',
+        'java',
+        'python',
         'rust',
         'toml',
         'yaml'
