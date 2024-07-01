6e002e4e3019170a5497d731f266d099f0c592d0
Esteban Borai <estebanborai@gmail.com>
2024-07-01 19:03:10 -0400

    fix: icons breaks on mobile

--- src/theme/NavbarItem/GitHubStars/index.tsx
+++ src/theme/NavbarItem/GitHubStars/index.tsx
@@ -4,7 +4,7 @@ import styles from './styles.module.css';
 
 export default function GitHubStars(): JSX.Element {
   return (
-    <li className={styles.githubStars}>
+    <li className={`${styles.githubStars} custom-button`}>
         <a href="https://gitHub.com/infinyon/fluvio/" target="_blank">
           <img src="https://img.shields.io/github/stars/infinyon/fluvio?style=social" alt="GitHub stars"/>
         </a>
