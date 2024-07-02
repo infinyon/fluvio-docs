f09f2d5bd5a10628e437436aa2bde630c333f246
Esteban Borai <estebanborai@gmail.com>
2024-07-02 15:27:34 -0400

    feat: projects using fluvio

--- src/components/Landing/components/Projects/GitHubRepository.tsx
+++ src/components/Landing/components/Projects/GitHubRepository.tsx
@@ -1,14 +1,23 @@
+import GitHub from "../icons/GitHub";
+
 export function GitHubRepository(props: {
   owner: string;
   name: string;
+  description: string;
 }): JSX.Element {
   return (
-    <article className="border border-solid border-gray-100 rounded-md p-4 h-20 w-full md:w-1/3 max-w-[300px]">
-      <a href={`https://github.com/${props.owner}/${props.name}`}>
-        <span>{props.owner}</span >
-        {`/`}
-        <span>{props.name}</span>
-      </a>
+    <article className="bg-zinc-100 dark:bg-zinc-800 border border-solid border-zinc-200 dark:border-zinc-800 rounded-md p-4 max-h-48 w-full md:w-1/3 max-w-[300px]">
+      <div className="flex items-center">
+        <figure className="flex justify-center items-center h-6 w-6 m-0 mr-2">
+          <GitHub className="text-black dark:text-white" />
+        </figure>
+        <a className="text-black dark:text-white hover:text-black hover:underline text-lg" href={`https://github.com/${props.owner}/${props.name}`}>
+          <span>{props.owner}</span>
+          {`/`}
+          <span className="font-semibold">{props.name}</span >
+        </a >
+      </div >
+      <p className="truncate">{props.description}</p >
     </article>
   );
 }
--- src/components/Landing/components/Projects/index.tsx
+++ src/components/Landing/components/Projects/index.tsx
@@ -4,10 +4,13 @@ export function Projects(): JSX.Element {
   return (
     <section className="flex flex-col items-center justify-center p-6">
       <h3 className="text-2xl font-bold">Projects Using Fluvio</h3>
-      <div className="flex justify-center items-center md:flex-row flex-col md:space-x-6 md:space-y-0 space-y-4">
-        <GitHubRepository owner="Hearth-Industries" name="Hearth" />
-        <GitHubRepository owner="Hearth-Industries" name="Hearth" />
-        <GitHubRepository owner="Hearth-Industries" name="Hearth" />
+      <div className="flex flex-wrap justify-center items-center md:flex-row flex-col gap-6">
+        <GitHubRepository owner="Hearth-Industries" name="Hearth" description="Hearth is a LavaLink Alternative written in Rust. That uses 30X less memory, and is almost in a state of feature parity." />
+        <GitHubRepository owner="Hearth-Industries" name="Hearth" description="Hearth is a LavaLink Alternative written in Rust. That uses 30X less memory, and is almost in a state of feature parity." />
+        <GitHubRepository owner="Hearth-Industries" name="Hearth" description="Hearth is a LavaLink Alternative written in Rust. That uses 30X less memory, and is almost in a state of feature parity." />
+        <GitHubRepository owner="Hearth-Industries" name="Hearth" description="Hearth is a LavaLink Alternative written in Rust. That uses 30X less memory, and is almost in a state of feature parity." />
+        <GitHubRepository owner="Hearth-Industries" name="Hearth" description="Hearth is a LavaLink Alternative written in Rust. That uses 30X less memory, and is almost in a state of feature parity." />
+        <GitHubRepository owner="Hearth-Industries" name="Hearth" description="Hearth is a LavaLink Alternative written in Rust. That uses 30X less memory, and is almost in a state of feature parity." />
       </div>
     </section>
   );
