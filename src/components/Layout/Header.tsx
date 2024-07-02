8e5035b6facfeb5ecd7802de8786e5f14b8776ba
Esteban Borai <estebanborai@gmail.com>
2024-07-01 20:01:59 -0400

    fix: infinyon logo should navigate to infinyon.com (#73)

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Header() {
   return (
     <header className="border-b border-solid border-gray-200 dark:border-transparent flex justify-between items-center py-2 px-4">
-      <a className='h-full flex flex-col justify-center items-center' href='/'>
+      <a className='h-full flex flex-col justify-center items-center' href='https://infinyon.com' target='_blank'>
         <img src="/img/infinyon-with-text.svg" className='dark:hidden' height={24} />
         <img src="/img/infinyon-with-text-white.svg" className='hidden dark:inline-block' height={24} />
       </a>
