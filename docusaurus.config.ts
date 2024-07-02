95c3eaaad90d5c60e784ee70f474ddb2c15d9231
Esteban Borai <estebanborai@gmail.com>
2024-07-02 13:22:13 -0400

    feat: bypass link checks

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -22,7 +22,7 @@ const config: Config = {
   organizationName: "InfinyOn", // Usually your GitHub org/user name.
   projectName: "fluvio", // Usually your repo name.
 
-  onBrokenLinks: "throw",
+  onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
