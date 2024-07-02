eecb57bef32831e1c2768a21c0f2085590774911
Esteban Borai <estebanborai@gmail.com>
2024-07-02 17:18:13 -0400

    feat: changes with team

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <header style={{ borderBottom: '1px solid #ddd' }} className="dark:!border-transparent flex justify-between items-center py-2 px-4">
+    <header style={{ borderBottom: '1px solid #ddd' }} className="py-2 dark:!border-transparent flex justify-between items-center px-4">
       <a className='h-full flex flex-col justify-center items-center' href='https://infinyon.com' target='_blank'>
         <img src="/img/infinyon-with-text.svg" className='dark:hidden' height={24} />
         <img src="/img/infinyon-with-text-white.svg" className='hidden dark:inline-block' height={24} />
       </a>
-      <a className="btn btn-secondary" target='_blank' href='https://infinyon.cloud/ui'>
+      <a className="btn btn-secondary btn-thin" target='_blank' href='https://infinyon.cloud/ui'>
         Login to InfinyOn Cloud
       </a>
     </header >
