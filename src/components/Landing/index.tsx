048dd8c5717604e60bb4ebce77072c39b3f68afc
Esteban Borai <estebanborai@gmail.com>
2024-07-10 16:29:19 -0400

    feat: introduce a docs main site (#95)
    ## Demo
    
    <img width="1507" alt="Screenshot 2024-07-10 at 4 26 13 PM"
    src="https://github.com/infinyon/fluvio-docs/assets/34756077/45fc395e-000a-4db4-8660-2a3e3869f6f7">

--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -1,14 +1,11 @@
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 
 import { Hero } from './components/Hero';
 import { WhyFluvio } from './components/WhyFluvio';
 import { Contribute } from './components/Contribute';
-import { Projects } from './components/Projects';
 import { GitHubInsights } from './components/GitHubInsights';
 
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
       title="Fluvio Community Documentation"
