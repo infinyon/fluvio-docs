c21350f39674867df30d8b41eb80f7d9459ef89b
Esteban Borai <estebanborai@gmail.com>
2024-07-23 13:35:56 -0400

    feat: run on cloud button takes you to infinyon cloud (#125)

--- src/components/Landing/components/Products/Card.tsx
+++ src/components/Landing/components/Products/Card.tsx
@@ -4,12 +4,12 @@ export function Card(props: {
   imgSrc: string;
   imgAlt: string;
 }): JSX.Element {
-  const cardClass = ['py-8', 'flex', 'flex-col', 'md:flex-row', 'md:gap-6', props.flip ? 'md:flex-row-reverse' : 'md:flex-row'];
+  const cardClass = ['py-8', 'px-4', 'flex', 'flex-col', 'md:flex-row', 'md:gap-6', props.flip ? 'md:flex-row-reverse' : 'md:flex-row'];
 
   return (
     <article className={cardClass.join(' ')}>
-      <figure className="bg-gray-50 dark:bg-zinc-900 border border-solid border-gray-100 dark:border-zinc-950 rounded flex items-center justify-center p-6 md:py-12 w-full md:w-[500px]">
-        <img src={props.imgSrc} alt={props.imgAlt} />
+      <figure className="m-0 bg-gray-50 dark:bg-zinc-900 border border-solid border-gray-100 dark:border-zinc-950 rounded flex items-center justify-center p-6 md:py-12 w-11/12 md:max-w-[500px]">
+        <img src={props.imgSrc} alt={props.imgAlt} className="w-full" />
       </figure>
       <div className="w-full md:w-8/12 p-6">
         {props.children}
