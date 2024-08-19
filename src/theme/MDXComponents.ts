c54a14bede74432ada2d6f9f5fe237a326d51fd7
Esteban Borai <estebanborai@gmail.com>
2024-08-19 17:07:18 -0400

    feat: use `tip` over `Idea` for consistency (#194)

--- src/theme/MDXComponents.ts
+++ src/theme/MDXComponents.ts
@@ -1,15 +1,9 @@
 import MDXComponents from '@theme-original/MDXComponents';
 
-import Alert from '../components/Quote/Alert';
-import Caution from '../components/Quote/Caution';
 import FAQBox from '../components/FAQBox';
-import Idea from '../components/Quote/Idea';
 
 export default {
   // Re-use the default mapping
   ...MDXComponents,
-  Alert,
-  Caution,
   FAQBox,
-  Idea,
 };
