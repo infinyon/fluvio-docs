0864eb8b714f40eda285d145e7a274b39a104289
Leo Borai <estebanborai@gmail.com>
2025-02-06 15:28:29 -0300

    feat: docs sidebar footer with version and product name (#356)
    - **fix: run audit fixes from npm**
      
    
    - **feat: provide docsidebar**
      
    
    - **feat: provide footer with product and version**

---
+++ src/theme/DocSidebar/Desktop/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import clsx from 'clsx';
+import {useThemeConfig} from '@docusaurus/theme-common';
+import Logo from '@theme/Logo';
+import CollapseButton from '@theme/DocSidebar/Desktop/CollapseButton';
+import Content from '@theme/DocSidebar/Desktop/Content';
+import type {Props} from '@theme/DocSidebar/Desktop';
+
+import styles from './styles.module.css';
+import DocSidebarFooter from '@site/src/components/DocSidebarFooter';
+
+function DocSidebarDesktop({path, sidebar, onCollapse, isHidden}: Props) {
+  const {
+    navbar: {hideOnScroll},
+    docs: {
+      sidebar: {hideable},
+    },
+  } = useThemeConfig();
+
+  return (
+    <div
+      className={clsx(
+        styles.sidebar,
+        hideOnScroll && styles.sidebarWithHideableNavbar,
+        isHidden && styles.sidebarHidden,
+      )}>
+      {hideOnScroll && <Logo tabIndex={-1} className={styles.sidebarLogo} />}
+      <Content path={path} sidebar={sidebar} />
+      {hideable && <CollapseButton onClick={onCollapse} />}
+      <DocSidebarFooter />
+    </div>
+  );
+}
+
+export default React.memo(DocSidebarDesktop);
